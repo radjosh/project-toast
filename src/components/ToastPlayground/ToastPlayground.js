d9bcc4eea4c156c0da362b46b9a8180c53890085
Joshua M Smith <radjosh@gmail.com>
2024-09-23 15:23:37 -0400

    form instead of button for pop toast

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -21,108 +21,107 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const newToast = {
+            message: message,
+            variant: variant,
+            id: crypto.randomUUID(),
+          };
+          const nextToasts = [...toasts, newToast];
+          setToasts(nextToasts);
+          setForceShow(true);
+          setMessage("");
+          setVariant("notice");
+        }}
+      >
+        {forceShow && (
+          <VisuallyHidden forceShow={forceShow} setForceShow={setForceShow}>
+            <ToastShelf toasts={toasts} setToasts={setToasts} />
+          </VisuallyHidden>
+        )}
 
-      {forceShow && (
-        <VisuallyHidden forceShow={forceShow} setForceShow={setForceShow}>
-          <ToastShelf toasts={toasts} setToasts={setToasts} />
-        </VisuallyHidden>
-      )}
-
-      <div className={styles.controlsWrapper}>
-        <div className={styles.row}>
-          <label
-            htmlFor="message"
-            className={styles.label}
-            style={{ alignSelf: "baseline" }}
-          >
-            Message
-          </label>
-          <div className={styles.inputWrapper}>
-            <textarea
-              id="message"
-              className={styles.messageInput}
-              value={message}
-              onChange={(event) => setMessage(event.target.value)}
-            />
-          </div>
-        </div>
-
-        <div className={styles.row}>
-          <div className={styles.label}>Variant</div>
-          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <label htmlFor="variant-notice">
-              <input
-                id="variant-notice"
-                type="radio"
-                name="variant"
-                value="notice"
-                checked={variant === "notice"}
-                onChange={(event) => setVariant(event.target.value)}
-              />
-              notice
-            </label>
-
-            <label htmlFor="variant-warning">
-              <input
-                id="variant-warning"
-                type="radio"
-                name="variant"
-                value="warning"
-                checked={variant === "warning"}
-                onChange={(event) => setVariant(event.target.value)}
-              />
-              warning
-            </label>
-
-            <label htmlFor="variant-success">
-              <input
-                id="variant-success"
-                type="radio"
-                name="variant"
-                value="success"
-                checked={variant === "success"}
-                onChange={(event) => setVariant(event.target.value)}
-              />
-              success
-            </label>
-
-            <label htmlFor="variant-error">
-              <input
-                id="variant-error"
-                type="radio"
-                name="variant"
-                value="error"
-                checked={variant === "error"}
-                onChange={(event) => setVariant(event.target.value)}
-              />
-              error
-            </label>
-          </div>
-        </div>
-
-        <div className={styles.row}>
-          <div className={styles.label} />
-          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button
-              onClick={() => {
-                const newToast = {
-                  message: message,
-                  variant: variant,
-                  id: crypto.randomUUID(),
-                };
-                const nextToasts = [...toasts, newToast];
-                setToasts(nextToasts);
-                setForceShow(true);
-                setMessage("");
-                setVariant("notice");
-              }}
+        <div className={styles.controlsWrapper}>
+          <div className={styles.row}>
+            <label
+              htmlFor="message"
+              className={styles.label}
+              style={{ alignSelf: "baseline" }}
             >
-              {" "}
-              Pop Toast!
-            </Button>
+              Message
+            </label>
+            <div className={styles.inputWrapper}>
+              <textarea
+                id="message"
+                className={styles.messageInput}
+                value={message}
+                onChange={(event) => setMessage(event.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className={styles.row}>
+            <div className={styles.label}>Variant</div>
+            <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
+              <label htmlFor="variant-notice">
+                <input
+                  id="variant-notice"
+                  type="radio"
+                  name="variant"
+                  value="notice"
+                  checked={variant === "notice"}
+                  onChange={(event) => setVariant(event.target.value)}
+                />
+                notice
+              </label>
+
+              <label htmlFor="variant-warning">
+                <input
+                  id="variant-warning"
+                  type="radio"
+                  name="variant"
+                  value="warning"
+                  checked={variant === "warning"}
+                  onChange={(event) => setVariant(event.target.value)}
+                />
+                warning
+              </label>
+
+              <label htmlFor="variant-success">
+                <input
+                  id="variant-success"
+                  type="radio"
+                  name="variant"
+                  value="success"
+                  checked={variant === "success"}
+                  onChange={(event) => setVariant(event.target.value)}
+                />
+                success
+              </label>
+
+              <label htmlFor="variant-error">
+                <input
+                  id="variant-error"
+                  type="radio"
+                  name="variant"
+                  value="error"
+                  checked={variant === "error"}
+                  onChange={(event) => setVariant(event.target.value)}
+                />
+                error
+              </label>
+            </div>
+          </div>
+
+          <div className={styles.row}>
+            <div className={styles.label} />
+            <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
+              <Button type="submit"> Pop Toast!</Button>
+            </div>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }

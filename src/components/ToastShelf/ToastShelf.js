209971d9a662195976ce9142c363608ef529c9a6
Joshua M Smith <radjosh@gmail.com>
2024-09-24 08:29:37 -0400

    clean up comments

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -4,10 +4,6 @@ import Toast from "../Toast";
 import styles from "./ToastShelf.module.css";
 import { ToastContext } from "../ToastProvider";
 
-// MAKE FUNCTIONS TO ADD OR REMOVE TOASTS
-// PUT THEM INTO CONTEXT
-// THIS SOLVES THAT HORRENDOUS CIRCULAR DEPENDENCY PROPS HELL THING
-
 function ToastShelf() {
   const { toasts } = React.useContext(ToastContext);
   return (
